a434fe1a4f5b06be3c3b8ad9048e98e1b87c2372
Malin Andreasson <malandre@kth.se>
2025-02-04 17:10:01 +0100

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,6 @@
 export const API_URL = "https://api.weatherapi.com/v1/current.json";
-export const API_KEY = "key=33948c53fb1a4e53b9e124901242711";
+export const API_KEY = "key= " 
 
 export const API2_URL = "https://maps.googleapis.com/maps/api/geocode/json";
-export const API2_KEY = "key=AIzaSyAHXBWqCcykrZUktZeI1SHEZ3ILJ_4E4Ao";
+export const API2_KEY = "key="
+//keys removed bc public repo
